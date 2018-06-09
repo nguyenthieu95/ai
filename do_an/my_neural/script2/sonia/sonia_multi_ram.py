ce9bca7634cf8714a2e697318c8967503ef1f63a
ThieuNv <nguyenthieu2102@gmail.com>
2018-06-09 15:47:23 +0700

    Fix bug script2

--- do_an/my_neural/script2/sonia/sonia_multi_ram.py
+++ do_an/my_neural/script2/sonia/sonia_multi_ram.py
@@ -5,8 +5,8 @@ from model.utils import IOHelper
 from pandas import read_csv
 import tensorflow as tf
 
-data = [3, 5, 8, 10]
-list_number_data = [(11120, 13900, 0), (6640, 8300, 0), (4160, 5200, 0), (3280, 4100, 0)]
+data = [8, 10]
+list_number_data = [(4160, 5200, 0), (3280, 4100, 0)]
 
 for i in range(0, len(data)):
     pathsave = os.path.dirname(os.path.abspath(__file__)) + "/result/" + str(data[i]) + "m/multi_ram/"
