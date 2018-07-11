44df201a14aaaff2cb40a464119469f9e855ee49
Nhuan Tran <nhuantran@brodev.com>
2018-07-12 00:01:27 +0700

    update multiprocessing

--- do_an/cao_quyen/run_tn1_2_full_cpu.py
+++ do_an/cao_quyen/run_tn1_2_full_cpu.py
@@ -1,32 +1,48 @@
+from multiprocessing import JoinableQueue,Queue, Process,cpu_count
+from queue import Empty
+import time
+import logging
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import ParameterGrid
+
 from ga_flnn.model import Model as GAModel
 from flnn.flnn import FLNN
-import pandas as pd
-from multiprocessing import JoinableQueue, Process
-import time
+
+
 # parameters
 data_index = [5]
 list_idx = [(6640, 1660)]
 methods = ['FLNN', 'GA']
-queue = JoinableQueue()
+queue = Queue()
 # Consumer
-WORKER_POOLS = 6
+WORKER_POOLS = cpu_count()
 WORKERS = []
-is_finished = False
+FORMAT = '%(asctime)-15s %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
 
-def train_model(queue,is_finished):
-    while not queue.empty():
-        items = queue.get()
-        sw = items[0]
-        method = items[1]
-        if method == 'GA':
-            p = GAModel(dataset_original, idx[0], idx[1], sw, expand_func=expand_func, pop_size=pop_size, pc=pc,
-                    pm=pm, activation=activation, test_name = test_name, path_save_result = path_save_result)
-            p.train(epochs=epoch)
-        elif method == 'FLNN':
-            p = FLNN(dataset_original, idx[0], idx[0] + idx[1], sw, activation=activation, e_func=expand_func,
-                    learning_rate=learning_rate, batch_size=batch_size, beta=beta, test_name = test_name, path_save_result = path_save_result)
-            p.train(epochs=epoch)
-    is_finished = True
+
+def train_model(queue,name):
+    LOGGER.info("Start worker %s",name)
+    while True:
+        try:
+            item = queue.get(True,timeout=1)
+            pop_size = item["pop_size"]
+            pc = item["pc"]
+            pm = item["pm"]
+
+            if method == 'GA':
+                p = GAModel(dataset_original, idx[0], idx[1], sw, expand_func=expand_func, pop_size=pop_size, pc=pc,
+                        pm=pm, activation=activation, test_name = test_name, path_save_result = path_save_result)
+                p.train(epochs=epoch)
+            elif method == 'FLNN':
+                p = FLNN(dataset_original, idx[0], idx[0] + idx[1], sw, activation=activation, e_func=expand_func,
+                        learning_rate=learning_rate, batch_size=batch_size, beta=beta, test_name = test_name, path_save_result = path_save_result)
+                p.train(epochs=epoch)
+        except Empty as e:
+            break
 #Producer
 
 for index, dataindex in enumerate(data_index):
@@ -51,22 +67,26 @@ for index, dataindex in enumerate(data_index):
     beta = 0.05
 
     # GA
-    pop_size = 100
-    pc = 0.8
-    pm = 0.02
-    hyper_params = []
+    pop_size = np.arange(100,1000,step=10)
+    pc = np.arange(0.1,0.5,step=0.2)
+    pm = np.arange(0,1,1.0,step=0.1)
 
-    for method in methods:
-        for sw in sliding_windows:
-            hyper_params.append([method,sw])
-            queue.put_nowait([method,sw])
+    param_grid = {
+        "pop_size": pop_size,
+        "pc": pc,
+        "pm": pm
+    }
+    for item in list(param_grid):
+        queue.put_nowait(item)
 
 
 for i in range(WORKER_POOLS):
-    WORKERS[i] = Process(target=train_model,args=(queue,is_finished))
+    WORKERS.append(Process(target=train_model,args=(queue,"worker_%s"%i)))
+
+for i in range(WORKER_POOLS):    
     WORKERS[i].start()
-
-while not is_finished:
-    time.sleep(1)
+time.sleep(5)
 for i in range(WORKER_POOLS):
+    WORKERS[i].join()
+    LOGGER.info("Terminate workers")
     WORKERS[i].terminate()
